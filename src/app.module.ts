592df9bdddc1c1870d7242eb174deeb0e81cdedb
mengxiangyu <18322276773@163.com>
2023-05-24 20:28:37 +0800

    feat:加入环境变量

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { getConfig } from './utils';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [getConfig],
+      ignoreEnvFile: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
